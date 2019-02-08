5991b264b0152a06a3e22cc3eec9b917bc89fef8
Daione <daione.pavan@operacao.rcadigital.com.br>
2019-02-08 19:10:02 -0200

    trocando a versão do openlayers

--- openLayers/openLayers.js
+++ openLayers/openLayers.js
@@ -1,76 +1,87 @@
-function openLayers(){
-    const markerSource = new ol.source.Vector();
+var iconFeature = new ol.Feature({
+    geometry: new ol.geom.Point([0, 0]),
+    name: 'Null Island',
+    population: 4000,
+    rainfall: 500
+  });
 
-    var markerStyle = new ol.style.Style({
-      image: new ol.style.Icon(/** @type {olx.style.IconOptions} */ ({
-        anchor: [0.5, 46],
-        anchorXUnits: 'fraction',
-        anchorYUnits: 'pixels',
-        opacity: 0.75,
-        src: 'https://openlayers.org/en/v4.6.4/examples/data/icon.png'
-      }))
-    });
+  var iconStyle = new ol.style.Style({
+    image: new ol.style.Icon(/** @type {olx.style.IconOptions} */ ({
+      anchor: [0.5, 46],
+      anchorXUnits: 'fraction',
+      anchorYUnits: 'pixels',
+      src: 'https://openlayers.org/en/v4.6.5/examples/data/icon.png'
+    }))
+  });
 
-   /* var map = new ol.Map({
-        target: 'mapOpenLayers',
-        layers: [
-          new ol.layer.Tile({
-            source: new ol.source.OSM()
-          })
-        ],
-        view: new ol.View({
-          center: ol.proj.fromLonLat([-51.23044788782864, -30.02878590159885]),
-          zoom: 10
-        })
-      });
+  iconFeature.setStyle(iconStyle);
 
-      map.on("click", ()=>{
-        console.log(map.getEventCoordinate(event)); 
-      }); 
+  var vectorSource = new ol.source.Vector({
+    features: [iconFeature]
+  });
 
-      var markers = new ol(); 
-      console.log('Markers ===> ', markers)
-*/
-let map = new ol.Map({
-    target: 'mapOpenLayers',
-    layers: [
-      new ol.layer.Tile({
-        source: new ol.source.OSM(),
-      }),
-      new ol.layer.Vector({
-        source: markerSource,
-        style: markerStyle,
-      }),
-    ],
-    view: new ol.View({
-      center: ol.proj.fromLonLat([6.661594, 50.433237]),
-      zoom: 3,
+  var vectorLayer = new ol.layer.Vector({
+    source: vectorSource
+  });
+
+  var rasterLayer = new ol.layer.Tile({
+    source: new ol.source.TileJSON({
+      url: 'https://api.tiles.mapbox.com/v3/mapbox.geography-class.json?secure',
+      crossOrigin: ''
     })
   });
-  
-  
-  function addMarker(lon, lat) {
-    console.log('lon:', lon);
-    console.log('lat:', lat);
-  
-    var iconFeatures = [];
-  
-    var iconFeature = new ol.Feature({
-      geometry: new ol.geom.Point(ol.proj.transform([lon, lat], 'EPSG:4326',
-        'EPSG:3857')),
-      name: 'Null Island',
-      population: 4000,
-      rainfall: 500
-    });
-  
-    markerSource.addFeature(iconFeature);
-  }
-  
-  map.on('singleclick',function(event){
-    var lonLat = ol.proj.toLonLat(event.coordinate);
-    addMarker(lonLat[0], lonLat[1]);
+
+  var map = new ol.Map({
+    layers: [ new ol.layer.Tile({
+        source: new ol.source.OSM()
+      }), 
+      vectorLayer
+    ],
+    
+    target: document.getElementById('map'),
+    view: new ol.View({
+      center: [0, 0],
+      zoom: 3
+    })
   });
 
-}
+  var element = document.getElementById('popup');
 
-openLayers(); 
+  var popup = new ol.Overlay({
+    element: element,
+    positioning: 'bottom-center',
+    stopEvent: false,
+    offset: [0, -50]
+  });
+  map.addOverlay(popup);
+
+  // display popup on click
+  map.on('click', function(evt) {
+    var feature = map.forEachFeatureAtPixel(evt.pixel,
+        function(feature) {
+          return feature;
+        });
+    if (feature) {
+      var coordinates = feature.getGeometry().getCoordinates();
+      popup.setPosition(coordinates);
+      $(element).popover({
+        'placement': 'top',
+        'html': true,
+        'content': feature.get('name')
+      });
+      $(element).popover('show');
+    } else {
+      $(element).popover('destroy');
+    }
+  });
+
+  // change mouse cursor when over marker
+  map.on('pointermove', function(e) {
+    if (e.dragging) {
+      $(element).popover('destroy');
+      return;
+    }
+    var pixel = map.getEventPixel(e.originalEvent);
+    var hit = map.hasFeatureAtPixel(pixel);
+    map.getTarget().style.cursor = hit ? 'pointer' : '';
+  });
