7a2efcb3899edac8df64af490d3875626594afc5
Daione <daione.pavan@operacao.rcadigital.com.br>
2019-02-11 16:40:01 -0200

    mais uma versao

--- openLayers/openLayers.js
+++ openLayers/openLayers.js
@@ -1,30 +1,73 @@
 var arrayTest = [];
-
+var map = new ol.Map({
+  layers: [new ol.layer.Tile({ source: new ol.source.OSM() }),],
+  target: document.getElementById('map'),
+  view: new ol.View({
+    center: ol.proj.fromLonLat([-51.22822165500112, -30.026267070739657]),
+    zoom: 14
+  })
+});
 
 estacoes.forEach(element => {
   var estacao = new ol.Feature({
     geometry: new ol.geom.Point(ol.proj.fromLonLat([parseFloat(element.long), parseFloat(element.lat)]))
   });
-
-  var ide = `estacao${element.id}`
-  debugger;
   estacao.setStyle(new ol.style.Style({
     image: new ol.style.Icon(/** @type {olx.style.IconOptions} */({
       color: '#8959A8',
       crossOrigin: 'anonymous',
-      src: 'https://openlayers.org/en/v4.6.5/examples/data/icon.png',
-      content: '<h1 class="teste"></h1>'
+      src: 'https://openlayers.org/en/v4.6.5/examples/data/icon.png'
+
     }))
   }));
 
-  $("#" + ide).on("click", function (event) {
-    console.log(event);
-  });
+  var divPopup = document.getElementById('popup')
+  map.on('click', function (evt) {
+    divPopup.innerHTML = '';
+    var feature = map.forEachFeatureAtPixel(evt.pixel, function (feature, layer) {
+      return feature;
+    });
 
-  estacao.addEventListener("click", ()=>{
-    alert('Eu funciono muito bem kkkk'+ element.endereco)
-  })
-  arrayTest.push(estacao);
+    if (feature) {
+      var coord = feature.getGeometry().getCoordinates();
+      var props = feature.getProperties();
+      var info = "<h2>bvnvb</h2>";
+      var element = document.getElementById('popup');
+      var popup = new ol.Overlay({
+        element: divPopup,
+        positioning: 'bottom-center',
+        stopEvent: false
+      });
+      // Offset the popup so it points at the middle of the marker not the tip
+      popup.setOffset([0, -22]);
+      popup.show(coord, info);
+    } else {
+      var url = estacao
+        .getSource()
+        .getGetFeatureInfoUrl(
+          evt.coordinate,
+          map.getView().getResolution(),
+          map.getView().getProjection(), {
+            'INFO_FORMAT': 'application/json',
+            'propertyName': 'total_budget'
+          }
+        );
+      reqwest({
+        url: url,
+        type: 'json',
+      }).then(function (data) {
+        var feature = data.features[0];
+        var props = feature.properties;
+        console.log(props.total_budget);
+        var info = "<h2>" + props.total_budget + "</h2>";
+        console.log(info);
+        popup.setPosition(evt.coordinate);
+        divPopup.innerHTML = info;
+      });
+    };
+
+    arrayTest.push(estacao);
+  });
 });
 
 var vectorSource = new ol.source.Vector({
@@ -43,131 +86,13 @@ var rasterLayer = new ol.layer.Tile({
   })
 });
 
-var map = new ol.Map({
-  layers: [new ol.layer.Tile({ source: new ol.source.OSM() }), vectorLayer],
-  target: document.getElementById('map'),
-  view: new ol.View({
-    center: ol.proj.fromLonLat([-51.22822165500112, -30.026267070739657]),
-    zoom: 14
-  })
-});
 
-map.on('click', function (evt) {
-  var feature = map.forEachFeatureAtPixel(evt.pixel,
-    function (feature, layer) {
-      return feature;
-    });
-  var popupContent = map.forEachLayerAtPixel(evt.pixel,
-    function (layer) {
-      return layer.get('content');
-    });
-  if (feature) {
-    popup.setPosition(evt.coordinate);
-    //---- new code to get the values of the features ------ 
-    var atts = feature.getProperties();
-    for (var prop in atts) {
-      var re = new RegExp("{" + prop + "}", "g");
-      var popupContent = popupContent.replace(re, atts[prop]);
-    }
-
-    $(popup_div).attr('data-placement', 'auto');
-    $(popup_div).attr('data-content', popupContent);
-    $(popup_div).attr('data-html', true);
-    $(popup_div).popover();
-    $(popup_div).popover('show');
-  } else {
-    $(popup_div).popover('destroy');
-  }
-});
+map.addLayer(vectorLayer);
 
 
+function makePopup(content) {
+  // alert('eu funciono'+ JSON.stringify(content));
+  return 'entrei no element: ' + JSON.stringify(content);
 
-/*
-var iconFeature = new ol.Feature({
-    geometry: new ol.geom.Point([0, 0]),
-    name: 'Null Island',
-    population: 4000,
-    rainfall: 500
-  });
+}
 
-  var iconStyle = new ol.style.Style({
-    image: new ol.style.Icon(/** @type {olx.style.IconOptions} */ /*({
-     chor: [0.5, 46],
-      anchorXUnits: 'fraction',
-      anchorYUnits: 'pixels',
-      src: 'https://openlayers.org/en/v4.6.5/examples/data/icon.png'
-    }))
-  });
-
-  iconFeature.setStyle(iconStyle);
-
-  var vectorSource = new ol.source.Vector({
-    features: [iconFeature]
-  });
-
-  var vectorLayer = new ol.layer.Vector({
-    source: vectorSource
-  });
-
-  var rasterLayer = new ol.layer.Tile({
-    source: new ol.source.TileJSON({
-      url: 'https://api.tiles.mapbox.com/v3/mapbox.geography-class.json?secure',
-      crossOrigin: ''
-    })
-  });
-
-  var map = new ol.Map({
-    layers: [ new ol.layer.Tile({
-        source: new ol.source.OSM()
-      }), 
-      vectorLayer
-    ],
-    
-    target: document.getElementById('map'),
-    view: new ol.View({
-      center: [0, 0],
-      zoom: 3
-    })
-  });
-
-  var element = document.getElementById('popup');
-
-  var popup = new ol.Overlay({
-    element: element,
-    positioning: 'bottom-center',
-    stopEvent: false,
-    offset: [0, -50]
-  });
-  map.addOverlay(popup);
-
-  // display popup on click
-  map.on('click', function(evt) {
-    var feature = map.forEachFeatureAtPixel(evt.pixel,
-        function(feature) {
-          return feature;
-        });
-    if (feature) {
-      var coordinates = feature.getGeometry().getCoordinates();
-      popup.setPosition(coordinates);
-      $(element).popover({
-        'placement': 'top',
-        'html': true,
-        'content': feature.get('name')
-      });
-      $(element).popover('show');
-    } else {
-      $(element).popover('destroy');
-    }
-  });
-
-  // change mouse cursor when over marker
-  map.on('pointermove', function(e) {
-    if (e.dragging) {
-      $(element).popover('destroy');
-      return;
-    }
-    var pixel = map.getEventPixel(e.originalEvent);
-    var hit = map.hasFeatureAtPixel(pixel);
-    map.getTarget().style.cursor = hit ? 'pointer' : '';
-  });
-*/
